f938e2addc64ad2730b2f2f713a988e0e0b5d443
Martin Sand Christensen <martin.sand.christensen@gmail.com>
2017-01-17 22:58:42 +0100

    If you can't get a RabbitMQ connection right away, retry.

--- pyservice/mq.py
+++ pyservice/mq.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 import contextlib
+from logsetup import log
 import pika
+import time
 
 @contextlib.contextmanager
 def channel(host='rabbitmq'):
     """Get a RabbitMQ channel."""
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
-    channel = connection.channel()
+    while True:
+        try:
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=host))
+            channel = connection.channel()
+        except:
+            log.error("Couldn't connect to RabbitMQ; retrying in a bit.")
+            sleep.sleep(5)
+        else:
+            break
     yield channel
     try:
         channel.close()
