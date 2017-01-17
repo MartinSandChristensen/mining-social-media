72c6ab0a5c6a98f36307c8874770d62d2566c302
Martin Sand Christensen <martin.sand.christensen@gmail.com>
2017-01-17 23:00:51 +0100

    Logging now actually useful.

--- pyservice/logsetup.py
+++ pyservice/logsetup.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 # Shared logging setup
 
+import inspect
 import logging
 import sys
 
-log = logging.getLogger(__name__)
-log.addHandler(logging.StreamHandler(sys.stderr))
+log = logging.getLogger(inspect.stack()[1][1].rstrip('.py'))
+handler = logging.StreamHandler(sys.stderr)
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.setLevel(logging.INFO)
